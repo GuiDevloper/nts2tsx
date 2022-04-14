4317a4d8d90a5cefc7c2104ae99555fd1fc67458
GuiDevloper <guicorrea384@gmail.com>
2022-04-14 16:13:47 -0300

    :sparkles: Add backup and restore features

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ if (cmds.includes('--invert')) {
   cmds.splice(cmds.indexOf('--invert'), 1)
 }
 
+const saveFile = path.join(process.cwd(), './nts2tsx-save.txt')
 const renamedFiles = []
 
 function renameAll(dirName) {
@@ -36,11 +37,45 @@ function renameAll(dirName) {
   })
 }
 
+function getStoredFiles() {
+  return (
+    fs.existsSync(saveFile)
+    ? fs.readFileSync(saveFile, { encoding: 'utf-8' }) + '\n'
+    : ''
+  )
+}
+
+function undoRename() {
+  const storedFiles = getStoredFiles().split('\n')
+  storedFiles.pop()
+  if (storedFiles.length === 0) {
+    return console.log('No files to be restored!')
+  }
+  storedFiles.forEach(file => {
+    fs.renameSync(file, file.replace(new RegExp(`\\${newExt}$`), oldExt))
+  })
+  console.log(`${storedFiles.length} files restored!`)
+  fs.unlinkSync(saveFile)
+  console.log('SaveFile erased!')
+}
+
+function storeUpdated() {
+  if (renamedFiles.length === 0) return
+  const oldUpdated = getStoredFiles()
+  fs.writeFileSync(saveFile, oldUpdated + renamedFiles.join('\n'))
+  console.log(`${renamedFiles.length} changed files saved!`)
+}
+
 function run() {
+  if (cmds.includes('--restore')) {
+    cmds.splice(cmds.indexOf('--restore'), 1)
+    return undoRename()
+  }
   renameAll(process.cwd())
   if (renamedFiles.length === 0) {
     return console.log('No files changed!')
   }
+  storeUpdated()
 }
 
 run()
